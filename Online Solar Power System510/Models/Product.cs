af66c1afb35c0523749c6e4e6449d25779301b40
Ayman510h <202220609@aau.edu.jo>
2026-01-13 22:43:00 +0200

    Enhance Product class with validation attributes
    Added data annotations for validation in Product class.

--- Online Solar Power System510/Models/Product.cs
+++ Online Solar Power System510/Models/Product.cs
@@ -1,10 +1,18 @@
-﻿namespace Online_Solar_Power_System510.Models
+using System.ComponentModel.DataAnnotations;
+
+namespace Online_Solar_Power_System510.Models
 {
-	public class Product
-	{
-		public int Id { get; set; }
-		public string Name { get; set; }
-		public decimal Price { get; set; }
-		public string Description { get; set; }
-	}
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [Range(1, 100000)]
+        public decimal Price { get; set; }
+
+        public string Description { get; set; }
+    }
 }
